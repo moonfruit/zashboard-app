c01e76d26fa51972d44d1901b714f19437bee9c9
MoonFruit <dkmoonfruit@gmail.com>
2025-11-24 16:00:58 +0800

    bug fix

--- main.go
+++ main.go
@@ -25,11 +25,10 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
-		Bind: []interface{}{
+		Bind: []any{
 			app,
 		},
 	})
-
 	if err != nil {
 		println("Error:", err.Error())
 	}
